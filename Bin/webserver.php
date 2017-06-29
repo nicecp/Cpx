fa0668c6e36dc60424c9e8f63ef8137377c8155f
nicecp <forward.nice.cp@gmail.com>
2017-06-29 15:13:10 +0800

    add composer

--- Bin/webserver.php
+++ Bin/webserver.php
@@ -3,9 +3,6 @@
  * WebServer 脚本
  */
 use Framework\Service;
-include_once __DIR__ . '/../Vendor/Bootstrap/Autoloader.php';
-
-// 注册自动加载器
-Bootstrap\Autoloader::instance()->clear()->setRoot('./')->setRoot('./Vendor/')->init();
+include_once __DIR__ . '/../Vendor/autoload.php';
 
 Service::run();
--- Controller/Index.php
+++ Controller/Index.php
@@ -6,8 +6,8 @@ use \Framework\Cpx;
 class Index extends Cpx {
 	public function index()
 	{
-		$this->request->getParams;
-		$list = $this->db('write')->select('*')->from('woman')->query();
+		// $this->request->getParams;
+		// $list = $this->db('write')->select('*')->from('woman')->query();
 		$list = array(
 			array('id'=>23, 'name'=>'pengc','phone'=>1500),
 			array('id'=>24, 'name'=>'kaixiangz', 'phone'=>1600),
